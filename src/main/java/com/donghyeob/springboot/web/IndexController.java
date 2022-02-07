9a0883d3b0b8784ac10f712316facd9b3590d565
donghyeob <donghyeob.k@gmail.com>
2022-02-07 11:57:33 +0900

    index controller & mustache

---
+++ src/main/java/com/donghyeob/springboot/web/IndexController.java
@@ -0,0 +1,14 @@
+package com.donghyeob.springboot.web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class IndexController {
+
+    @GetMapping("/")
+    public String index() {
+        // src/main/resources/templates/index.mustache로 변환되어 View Resolver가 처리하게 됨
+        return "index";
+    }
+}
